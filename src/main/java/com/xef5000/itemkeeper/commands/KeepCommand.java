ae9eedc0a3daf30f3bf087da150b00dbcd27911d
xef5000 <64156533+xef5000@users.noreply.github.com>
2023-11-28 18:28:39 -0500

    Works for 1.20.2

--- src/main/java/com/xef5000/itemkeeper/commands/KeepCommand.java
+++ src/main/java/com/xef5000/itemkeeper/commands/KeepCommand.java
@@ -1,11 +1,15 @@
 package com.xef5000.itemkeeper.commands;
 
+import com.cryptomorin.xseries.XItemStack;
+import com.cryptomorin.xseries.XMaterial;
 import com.xef5000.itemkeeper.utils.KeepUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
 
 public class KeepCommand implements CommandExecutor {
 
@@ -24,6 +28,13 @@ public class KeepCommand implements CommandExecutor {
 
         Player player = (Player) commandSender;
         ItemStack itemStack = player.getItemInHand();
+
+        XMaterial material = XMaterial.matchXMaterial(itemStack);
+        Map<String, Object> newstack = XItemStack.serialize(player.getItemInHand());
+
+        System.out.println("Newstack: "+newstack);
+        System.out.println("Newstackitem: " +XItemStack.deserialize(newstack));
+        System.out.println("comparison with old: "+itemStack);
 
 
         ItemStack newItem;
