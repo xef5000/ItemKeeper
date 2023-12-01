8526dd7d383f190570b9857c3d7c7bc5eac51bac
xef5000 <64156533+xef5000@users.noreply.github.com>
2023-11-30 19:51:06 -0500

    Added name change when item is marked as permanent

--- src/main/java/com/xef5000/itemkeeper/ItemKeeper.java
+++ src/main/java/com/xef5000/itemkeeper/ItemKeeper.java
@@ -3,6 +3,7 @@ package com.xef5000.itemkeeper;
 import com.xef5000.itemkeeper.commands.KeepCommand;
 import com.xef5000.itemkeeper.commands.ReloadCommand;
 import com.xef5000.itemkeeper.listeners.PlayerDeathListener;
+import com.xef5000.itemkeeper.utils.KeepUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ItemKeeper extends JavaPlugin {
@@ -22,6 +23,8 @@ public final class ItemKeeper extends JavaPlugin {
 
         config = new Config(this);
         config.init();
+
+        KeepUtils.setPlugin(this);
 
         getLogger().info("ItemKeeper has been enabled!");
     }
--- src/main/java/com/xef5000/itemkeeper/utils/KeepUtils.java
+++ src/main/java/com/xef5000/itemkeeper/utils/KeepUtils.java
@@ -1,10 +1,19 @@
 package com.xef5000.itemkeeper.utils;
 
+import com.xef5000.itemkeeper.ItemKeeper;
 import de.tr7zw.changeme.nbtapi.NBT;
 import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class KeepUtils {
+
+    private static ItemKeeper plugin;
+
+    public static void setPlugin(ItemKeeper plugin) {
+        KeepUtils.plugin = plugin;
+    }
 
     public static ItemStack makeItemKeepable(ItemStack itemStack, boolean value) {
         if (value) {
@@ -16,6 +25,55 @@ public class KeepUtils {
                 nbt.removeKey("keep");
             });
         }
+
+        return changeItemName(itemStack, value);
+    }
+
+    private static ItemStack changeItemName(ItemStack itemStack, boolean value) {
+        boolean prefix = plugin.getConfig().getBoolean("item.name-prefix");
+        boolean suffix = plugin.getConfig().getBoolean("item.name-suffix");
+
+        ItemMeta meta = itemStack.getItemMeta();
+        String displayName = (meta.hasDisplayName()) ? meta.getDisplayName() : WordUtils.capitalizeFully(itemStack.getType().name().replace("_", " "));
+        if ((suffix && !prefix) && !displayName.startsWith("§r")) displayName = "§r"+displayName;
+        if (prefix && displayName.startsWith("§r")) displayName = displayName.substring(2);
+
+        if (suffix) {
+            String suffixStringOn = plugin.getConfig().getString("item.suffix-on").replace("&", "§");
+            String suffixStringOff = plugin.getConfig().getString("item.suffix-off").replace("&", "§");
+
+            if (value) {  // When toggling to "ON"
+                if (displayName.endsWith(suffixStringOff)) {
+                    displayName = displayName.substring(0, displayName.length() - suffixStringOff.length()) + suffixStringOn;
+                } else {
+                    displayName = displayName + suffixStringOn;
+                }
+            } else {  // When toggling to "OFF"
+                if (displayName.endsWith(suffixStringOn)) {
+                    displayName = displayName.substring(0, displayName.length() - suffixStringOn.length()) + suffixStringOff;
+                }
+            }
+        }
+
+        if (prefix) {
+            String prefixStringOn = plugin.getConfig().getString("item.prefix-on").replace("&", "§");
+            String prefixStringOff = plugin.getConfig().getString("item.prefix-off").replace("&", "§");
+
+            if (value) { // When toggling to "ON"
+                if (displayName.startsWith(prefixStringOff)) {
+                    displayName = prefixStringOn + displayName.substring(prefixStringOff.length());
+                } else {
+                    displayName = prefixStringOn + displayName;
+                }
+            } else { // When toggling to "OFF"
+                if (displayName.startsWith(prefixStringOn)) {
+                    displayName = prefixStringOff + displayName.substring(prefixStringOn.length());
+                }
+            }
+        }
+
+        meta.setDisplayName(displayName);
+        itemStack.setItemMeta(meta);
         return itemStack;
     }
 
